12e7a3bb296d987039fbe228be9c3727e5b100b4
Tayeb Ahmed Nasir <tayeb13@Tayebs-MacBook-Pro.local>
2018-05-19 12:13:08 -0400

    keycode 13 added to init.js

--- init.js
+++ init.js
@@ -9,3 +9,10 @@ var weatherBox = document.querySelector('#weather');
 var weatherCity = weatherBox.firstElementChild;
 var weatherDescription = document.querySelector('#weatherDescription');
 var weatherTemperature = weatherBox.lastElementChild;
+document.getElementById('city')
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById('button').click();
+    }
+});
