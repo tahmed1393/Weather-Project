5428178055d627a44b0605e09b4eaa567106e547
Tayeb Ahmed Nasir <tayeb13@Tayebs-MacBook-Pro.local>
2018-05-19 23:08:09 -0400

    replacing button

--- init.js
+++ init.js
@@ -13,6 +13,6 @@ document.getElementById('city')
     .addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
-        document.getElementById('button').click();
+        searchWeather();
     }
-
+});
